80cc2d5e15773301c91cbdfa33e0554fddd2b6de
manuimfeld <manu_jadir@hotmail.com>
2024-08-08 15:51:46 -0300

    add jspdf, create pdf for saes, and add chart in sales/date

--- src/app/components/chart.js
+++ src/app/components/chart.js
@@ -9,6 +9,8 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { ChartContainer } from "@/components/ui/chart";
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
 
 const methodMapping = {
   1: "Efectivo",
@@ -17,7 +19,11 @@ const methodMapping = {
   4: "Crédito",
 };
 
-// Mapea los métodos de pago a colores
+const turnMethodMap = {
+  1: "Mañana",
+  2: "Tarde",
+};
+
 const colorMapping = {
   Efectivo: "hsl(191, 100%, 41%)",
   Transferencia: "hsl(193, 69%, 53%)",
@@ -25,7 +31,6 @@ const colorMapping = {
   Crédito: "hsl(195, 100%, 31%)",
 };
 
-// Configuración del gráfico
 const chartConfig = {
   quantity: {
     label: "Cantidad",
@@ -35,16 +40,16 @@ const chartConfig = {
   },
 };
 
-// Función para transformar los datos de ventas
 const transformSalesData = (sales) => {
   const groupedSales = sales.reduce((acc, sale) => {
     const method = methodMapping[sale.payment_method_id];
 
     if (!acc[method]) {
-      acc[method] = { quantity: 0 };
+      acc[method] = { quantity: 0, totalAmount: 0 };
     }
 
     acc[method].quantity += 1;
+    acc[method].totalAmount += Math.floor(parseFloat(sale.amount));
 
     return acc;
   }, {});
@@ -52,15 +57,110 @@ const transformSalesData = (sales) => {
   return Object.keys(groupedSales).map((method) => ({
     payment_method_id: method,
     quantity: groupedSales[method].quantity,
+    totalAmount: parseFloat(groupedSales[method].totalAmount), // Asegúrate de que totalAmount sea un número
     fill: colorMapping[method],
   }));
 };
 
-export function ChartPie({ data, totalAmount }) {
-  // Transformar los datos de ventas
-  const chartData = transformSalesData(data);
+const formatDateInSpanish = (date) => {
+  const options = { day: "numeric", month: "long", year: "numeric" };
+  return date.toLocaleDateString("es-ES", options);
+};
 
-  // Calcular el total de cantidad
+const generatePDF = (sales) => {
+  const pdf = new jsPDF();
+  const chartData = transformSalesData(sales);
+
+  // Configuración de texto
+  pdf.setFont("Helvetica", "normal");
+  pdf.setFontSize(12);
+
+  // Agregar título
+  pdf.setFontSize(20);
+  pdf.text("Reporte de Ventas - Bichitos Petshop", 14, 25);
+
+  // Agregar fecha
+  const date = new Date();
+  const formattedDate = formatDateInSpanish(date);
+  pdf.setFontSize(16);
+  pdf.text(formattedDate, 14, 35);
+
+  // Resumen de ventas
+  pdf.setFontSize(14);
+  const totalSalesText = `Ventas en total: ${sales.length}`;
+  pdf.text(totalSalesText, 14, 50);
+  const totalAmount = sales.reduce(
+    (sum, sale) => sum + Math.floor(parseFloat(sale.amount)),
+    0
+  );
+  const totalAmountText = `Monto total: ${totalAmount.toLocaleString()}`;
+  pdf.text(totalAmountText, 14, 58);
+
+  // Detalles agrupados
+  pdf.setFontSize(14);
+  pdf.setTextColor(0, 0, 255);
+  pdf.text("Resumen por Tipo de Pago", 14, 75);
+  pdf.setFontSize(12);
+  pdf.setTextColor(0, 0, 0);
+
+  // Usar autoTable para crear una tabla
+  pdf.autoTable({
+    startY: 80,
+    head: [["Método de Pago", "Cantidad", "Monto Total"]],
+    body: chartData.map((data) => [
+      data.payment_method_id,
+      data.quantity,
+      data.totalAmount,
+    ]),
+    styles: {
+      fillColor: [240, 240, 240], // Fondo de las celdas
+      textColor: [0, 0, 0], // Color del texto
+      lineWidth: 0.1, // Grosor de las líneas
+      fontSize: 10,
+    },
+    headStyles: {
+      fillColor: [0, 123, 255], // Color de fondo del encabezado
+      textColor: [255, 255, 255], // Color del texto del encabezado
+    },
+    margin: { left: 14, right: 14 },
+    pageBreak: "auto", // Manejo de páginas
+  });
+
+  // Detalles individuales de ventas
+  pdf.setFontSize(14);
+  pdf.setTextColor(0, 0, 255);
+  pdf.text("Detalles de Ventas", 14, pdf.autoTable.previous.finalY + 10);
+  pdf.setFontSize(12);
+  pdf.setTextColor(0, 0, 0);
+
+  pdf.autoTable({
+    startY: pdf.autoTable.previous.finalY + 20,
+    head: [["Monto", "Tipo de Pago", "Turno"]],
+    body: sales.map((sale) => [
+      parseFloat(sale.amount).toFixed(2), // Asegúrate de mostrar dos decimales
+      methodMapping[sale.payment_method_id],
+      turnMethodMap[sale.turn] || "Desconocido",
+    ]),
+    styles: {
+      fillColor: [240, 240, 240], // Fondo de las celdas
+      textColor: [0, 0, 0], // Color del texto
+      lineWidth: 0.1, // Grosor de las líneas
+      fontSize: 10,
+    },
+    headStyles: {
+      fillColor: [0, 123, 255], // Color de fondo del encabezado
+      textColor: [255, 255, 255], // Color del texto del encabezado
+    },
+    margin: { left: 14, right: 14 },
+    pageBreak: "auto", // Manejo de páginas
+  });
+
+  // Guardar PDF
+  pdf.save("sales-report.pdf");
+};
+
+export function ChartPie({ data, totalAmount }) {
+  const chartData = transformSalesData(data);
   const totalQuantity = chartData.reduce(
     (sum, entry) => sum + entry.quantity,
     0
@@ -129,7 +229,7 @@ export function ChartPie({ data, totalAmount }) {
           El monto total es de: {totalAmount}
         </div>
         <div className="leading-none text-muted-foreground">
-          <Button>Generar PDF</Button>
+          <Button onClick={() => generatePDF(data)}>Generar PDF</Button>
         </div>
       </CardFooter>
     </Card>
