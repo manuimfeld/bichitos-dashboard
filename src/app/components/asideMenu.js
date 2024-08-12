a4177c4a2ed97e029608c709ae110160afc146a4
manuimfeld <manu_jadir@hotmail.com>
2024-08-12 17:34:16 -0300

    replace stroke-width with strokeWidth, and linecap & linejoin

--- src/app/components/asideMenu.js
+++ src/app/components/asideMenu.js
@@ -20,9 +20,9 @@ export default function AsideMenu() {
             viewBox="0 0 24 24"
             fill="none"
             stroke="#000000"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
             <path d="M20 9v11a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9" />
             <path d="M9 22V12h6v10M2 10.6L12 2l10 8.6" />
